e0c1406033f82650e48dc73faf8e1003a10b8ed5
sahvsergio <sergioandresherrera@gmail.com>
2025-01-17 21:20:42 -0500

    add 9 list items, div to the DOM

--- js/script.js
+++ js/script.js
@@ -13,21 +13,65 @@ For assistance:
 
 //mapping out all initial elements for later use
 const header= document.querySelector('header')
-const studentUl=document.querySelector('.student-list');
+
 const paginationDiv=document.querySelector(".pagination")
-const linkList=document.querySelector(.'link-list')
-
-
+const linkList=document.querySelector('.link-list')
+const itemsPerPage=9;
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 function showPage(list, page){
-   console.log('showPage');
+   
+   let startIndex=(page*itemsPerPage)-itemsPerPage;
+   let EndIndex=page*itemsPerPage;
+   const studentUL=document.querySelector('.student-list');
+   studentUL.innerHtml='';
+
+   for (let i=0; i<data.length; i++){
+     
+      
+      if (i>=startIndex &&i<EndIndex){
+         let studentLi=document.createElement('li');
+         let studentDetails=document.createElement('div');
+         studentLi.className='student-item cf';
+         studentDetails.className='student-details';
+         studentUL.appendChild(studentLi);
+         studentLi.appendChild(studentDetails) ;
+        
+         
+         
+
+      }   
 
 
+   
+      
+
+   }
+   return studentUL;
 }
+  
+      
+      //
+
+      //<li class="student-item cf">
+      //    <div class="student-details">
+      //      <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
+      //      <h3>Ethel Dean</h3>
+      //      <span class="email">ethel.dean@example.com</span>
+      //    </div>
+      //    <div class="joined-details">
+      //      <span class="date">Joined 12-15-2005</span>
+      //    </div>
+      //  </li>
+
+ 
+   
+
+
+
 
 
 
@@ -35,12 +79,9 @@ function showPage(list, page){
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-function addPagination(){
-   console.log('Add pagination');
 
-
-}
 
 
 
 // Call functions
+showPage(data, 3);
